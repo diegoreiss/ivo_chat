9dc452b65e03e8c087e5d312944c8840d3aba018
diegoreiss <diegoportal50@gmail.com>
2024-04-28 17:32:59 -0300

    habilitando cache database com redis

--- IVOChat/IVOChat/settings.py
+++ IVOChat/IVOChat/settings.py
@@ -174,12 +174,15 @@ ASGI_APPLICATION = 'IVOChat.asgi.application'
 
 # Redis Cache
 
-# CACHES = {
-#     "default": {
-#         "BACKEND": "django.core.cache.backends.redis.RedisCache",
-#         "LOCATION": "redis://127.0.0.1:6379",
-#     }
-# }
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },
+    },
+}
 
 
 # Database
