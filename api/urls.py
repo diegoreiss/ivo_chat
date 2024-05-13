c4315d1ab50a0d9e5cc500371e3ea20753a17087
Diego Reis <diegoportal.reis@outlook.com>
2024-05-12 23:46:07 -0300

    reconstruindo as urls

--- api/urls.py
+++ api/urls.py
@@ -4,13 +4,29 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from . import views
 
 
-urlpatterns = [
+token_urls = [
     path('token/', view=TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', view=TokenRefreshView.as_view(), name='token_refresh'),
     path('token/blacklist/', TokenBlacklistView.as_view(), name='token_blacklist'),
+]
+
+user_urls = [
     path('user/', view=views.CustomUserListCreate.as_view(), name='customuser_view_create'),
     path('user/role/aluno/', view=views.CustomUserByRoleAlunoAPIView.as_view(), name='customuser_by_role_admin_view'),
     path('user/current/', view=views.CustomUserCurrentRetrieve.as_view(), name='customuser_current_role_view'),
     path('user/<str:uuid>/role/aluno/update/password', view=views.CustomUserChangePasswordAPIView.as_view(), name='customuser_password_update'),
 ]
+
+bot_urls = [
+    path('bot/intent/', view=views.IntentListCreate.as_view(), name='intent_view_create'),
+    path('bot/intent/<str:intent>', view=views.IntentListBy.as_view(), name='intent_list_by'),
+    path('bot/intent/<str:intent>/change/examples', view=views.IntentUpdateExamples.as_view(), name='intent_update_examples'),
+]
+
+urlpatterns = [
+    *token_urls,
+    *user_urls,
+    *bot_urls
+]
+
 
