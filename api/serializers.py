e1b22faa1bc87a227ae78e805df71f0bb71e3bd5
Diego Reis <diegoportal.reis@outlook.com>
2024-05-19 01:05:32 -0300

    serializers para manipulação de responses

--- api/serializers.py
+++ api/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from drf_yasg import openapi
 
 from . import models
 
@@ -35,6 +36,9 @@ class IntentSerializer(serializers.Serializer):
 class IntentExamplesSerializer(serializers.Serializer):
     examples = serializers.CharField()
 
+class IntentNamesSerializer(serializers.Serializer):
+    data = serializers.ListField(child=serializers.CharField())
+
 class NLUSerializer(serializers.Serializer):
     total_pages = serializers.IntegerField()
     nlu = serializers.DictField(child=serializers.ListField(child=IntentSerializer()))
@@ -42,3 +46,86 @@ class NLUSerializer(serializers.Serializer):
 class UtterSerializer(serializers.Serializer):
     total_pages = serializers.IntegerField()
     responses = serializers.JSONField()
+
+class DynamicResponseSerializer(serializers.Serializer):
+    class Meta:
+        swagger_schema_fields = {
+            'type': openapi.TYPE_OBJECT,
+            'title': 'Response',
+            'properties': {
+                'utter_<nome_da_intent>': openapi.Schema(
+                    title='response name',
+                    type=openapi.TYPE_ARRAY,
+                    items=openapi.Schema(
+                        title='text of response',
+                        type=openapi.TYPE_OBJECT,
+                        properties={
+                            'text': openapi.Schema(
+                                type=openapi.TYPE_STRING,
+                                description='text of response'
+                            )
+                        }
+                    )
+                ),
+            },
+            'required': ['utter_<nome_da_intent>']
+        }
+    def to_representation(self, instance):
+        return instance
+    
+    def to_internal_value(self, data):
+        if not isinstance(data, dict):
+            raise serializers.ValidationError('Esperando um dicionário de dados')
+
+        if len(data) != 1:
+            raise serializers.ValidationError('Deve haver apenas uma chave no dicionário')
+        
+        key: str = list(data.keys())[0]
+
+        if not key.startswith('utter_'):
+            raise serializers.ValidationError('O valor da key deve começar com "utter_".')
+
+        if not isinstance(data[key], list):
+            raise serializers.ValidationError(f'O valor da key "{key}" deve ser uma lista')
+        
+        if not len(data[key]) or len(data[key]) > 1:
+            raise serializers.ValidationError('A chave não pode ter valores vazios e não pode passar de 1 item')
+        
+        for item in data[key]:
+            if not isinstance(item, dict) or 'text' not in item:
+                raise serializers.ValidationError(f'Cada item da key "{key}" deve ter um dicionário contendo a chave "text" ')
+        
+        return data
+    
+    def validate(self, data):
+        return data
+
+class ResponseTextsSerializer(serializers.Serializer):
+    class Meta:
+        swagger_schema_fields = {
+            'type': openapi.TYPE_OBJECT,
+            'title': 'Response Texts',
+            'properties': {
+                'texts': openapi.Schema(
+                    title='texts of response',
+                    type=openapi.TYPE_ARRAY,
+                    items=openapi.Schema(
+                        title='texts of response',
+                        type=openapi.TYPE_OBJECT,
+                        properties={
+                            'text': openapi.Schema(
+                                type=openapi.TYPE_STRING,
+                                description='text of response'
+                            )
+                        }
+                    )
+                )
+            }
+        }
+
+    texts = serializers.ListField(
+        child=serializers.DictField(
+            child=serializers.CharField()
+        )
+    )
+    
